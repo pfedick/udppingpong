9d552260ed6cfa46e94ce619777a91727b742d61
pfedick <patrick@pfp.de>
2020-01-17 09:42:28 +0100

    fixed tarfile

--- build_tar_gz.sh
+++ build_tar_gz.sh
@@ -17,15 +17,19 @@ create_dir ()
 rm -rf distfiles build/dist
 create_dir distfiles
 create_dir build/dist
+create_dir build/dist/udppingpong-$VERSION-$RPM_RELEASE
 
-tar -czf distfiles/udppingpong-$VERSION-$RPM_RELEASE.tar.gz *.m4 autoconf include/sender.h \
-	include/sensor.h include/config.h.in src *.TXT configure Makefile.in README.md \
+find *.m4 autoconf include/sender.h \
+	include/sensor.h include/udpecho.h include/config.h.in src *.TXT configure Makefile.in README.md \
 	ppl7/src ppl7/Makefile.in ppl7/*.TXT \
 	ppl7/include/*.in ppl7/include/ppl7.h ppl7/include/compat_ppl7.h \
 	ppl7/include/ppl7-algorithms.h ppl7/include/ppl7-crypto.h \
 	ppl7/include/ppl7-exceptions.h ppl7/include/ppl7-inet.h ppl7/include/ppl7-types.h \
-	ppl7/include/prolog_ppl7.h ppl7/include/socket_ppl7.h ppl7/include/threads_ppl7.h
+	ppl7/include/prolog_ppl7.h ppl7/include/socket_ppl7.h ppl7/include/threads_ppl7.h \
+	| cpio -pdmv ${SOURCE}/build/dist/udppingpong-$VERSION-$RPM_RELEASE
 
+cd ${SOURCE}/build/dist
+tar -czf ${SOURCE}/distfiles/udppingpong-$VERSION-$RPM_RELEASE.tar.gz udppingpong-$VERSION-$RPM_RELEASE
 	
 
 
